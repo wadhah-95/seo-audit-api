05bfd05672db1a7bc3a811c26eef5e75eb448441
wadhah hermessi <wadhah.hermessi@enicar.ucar.tn>
2026-06-30 15:10:06 +0100

    feat: calculate SEO score from recommendations

--- src/services/audit.service.ts
+++ src/services/audit.service.ts
@@ -2,17 +2,19 @@ import { normalizeUrl } from "../utils/url.util";
 import { fetchHtml } from "./fetch.service";
 import { analyzeSeo } from "./seo-analyzer.service";
 import {generateRecommendations} from "./recommendation.service";
+import { calculateScore } from "./scoring.service";
 
 export async function createAudit(rawUrl: string){
   let normalizedUrl=normalizeUrl(rawUrl);
   const fetchedPage=await fetchHtml(normalizedUrl);
   const analysis=analyzeSeo(fetchedPage.html);
   const recommendations=generateRecommendations(analysis);
+  const score=calculateScore(recommendations);
   //console.log("NEW createAudit service is running");
   return {
     id: "fake-audit-id",
     url: normalizedUrl,
-    score: 85,
+    score,
     htmlLength: fetchedPage.html.length,
     statusCode: fetchedPage.statusCode,
     analysis,
