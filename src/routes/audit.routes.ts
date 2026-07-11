5713a47586de6e5d7cee10b79f03fd364e633de0
wadhah hermessi <wadhah.hermessi@enicar.ucar.tn>
2026-07-11 10:40:41 +0100

    feature/website-crawler/created file

--- src/routes/audit.routes.ts
+++ src/routes/audit.routes.ts
@@ -1,7 +1,12 @@
+//import router
 import {Router} from "express";
+//import functions from controllers folder
 import{createAuditController, getAuditsController, getAuditByIdController} from "../controllers/audit.controller";
+//create route
 const router=Router();
+//route requests
 router.post("/", createAuditController);
 router.get("/", getAuditsController);
 router.get("/:id", getAuditByIdController);
+//export route
 export default router;
